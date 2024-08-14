4c189c087f415143d303051468b6374230b76912
NG291 <you@example.com>
2024-08-15 06:10:52 +0700

    thuc hanh 2

---
+++ src/main/java/com/module4/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.module4.controller;
+
+
+import com.module4.model.Login;
+import com.module4.model.User;
+import com.module4.service.UserDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("login", new Login());
+        return modelAndView;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+
+        User user = UserDAO.checkLogin(login);
+        ModelAndView modelAndView;
+        if(user == null){
+            modelAndView = new ModelAndView("error");
+        } else {
+            modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+        }
+        return modelAndView;
+    }
+}
